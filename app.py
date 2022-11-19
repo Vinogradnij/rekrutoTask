028be16595c635c94bfbdd58346823b98a4d7558
Vinogradnij <vlud5@mail.ru>
2022-11-19 19:04:49 +0300

    Реализация вывода через url вместо form

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 @app.route('/hello', methods=['GET'])
 def hello_world():
-    name = '' if 'name' not in request.args else request.args['name']
-    message = '' if 'message' not in request.args else request.args['message']
-    return render_template('hello.html', name=name, message=message)
+    name = 'Rekruto' if 'name' not in request.args else request.args['name']
+    message = 'Давай дружить' if 'message' not in request.args else request.args['message']
+    return f'{name}!{message}!'
 
 
 if __name__ == '__main__':
